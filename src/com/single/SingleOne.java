589f51f76642cd856e3c4e914105d2a60ba79d25
FayeWongBinder <549014869@qq.com>
2020-11-15 15:51:51 +0800

    single

--- src/com/single/SingleOne.java
+++ src/com/single/SingleOne.java
@@ -1,5 +1,17 @@
 package com.single;
-
+/**
+ * ¶öººÄ£Ê½
+ * @author 54901
+ *
+ */
 public class SingleOne {
+	
+	private static SingleOne sInstance=new SingleOne();
+	private SingleOne() {
+		
+	}
+	public static SingleOne getInstance() {
+		return sInstance;
+	}
 
 }
